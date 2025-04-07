225c8824eda7d8163246bd45018b8e7e27fec722
Simon Massey <simon@simonmassey.org>
2025-04-07 15:52:02 +0100

    fix: Remove unnecessary blank lines in server test file

--- tests/server_test.ts
+++ tests/server_test.ts
@@ -1,8 +1,6 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { handler } from "../server.ts";
 
-
-
 Deno.test("Server returns import map", async () => {
   const req = new Request("http://localhost:8000/my-app/import-map.json");
   const response = await handler(req);
