4a7546b439371ace9e0599d13eed84c152f99425
Simon Massey <simon@simonmassey.org>
2025-04-07 15:31:29 +0100

    refactor: Replace readTextFile with Deno.readTextFile in server test

--- tests/server_test.ts
+++ tests/server_test.ts
@@ -1,6 +1,7 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { handler } from "../server.ts";
-import { readTextFile } from "https://deno.land/std@0.224.0/fs/read_text_file.ts";
+
+
 
 Deno.test("Server returns import map", async () => {
   const req = new Request("http://localhost:8000/my-app/import-map.json");
@@ -11,7 +12,7 @@ Deno.test("Server returns import map", async () => {
 Deno.test("Server returns correct import map content", async () => {
   const req = new Request("http://localhost:8000/my-app/import-map.json");
   const response = await handler(req);
-  const expected = await readTextFile("default-import-map.json");
+  const expected = await Deno.readTextFile("default-import-map.json");
   const actual = await response.text();
   assertEquals(actual, JSON.stringify(JSON.parse(expected), null, 2));
 });
